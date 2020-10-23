78dc255840ff2beb6a89fcd326719920cd7f3096
Frogora <rr4428310@gmail.com>
2020-10-23 17:52:46 +0530

    pushing Completed project

---
+++ HeartBeat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp2
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+
+        public HeartBeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            // 1 sec interval , it will start after every 1 sec
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Temp\Demos\HeartBeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                Console.WriteLine("Hello World!");
+                var exitCode = HostFactory.Run(x =>
+                {
+                    x.Service<HeartBeat>(s =>
+                    {
+                        s.ConstructUsing(heartbeat => new HeartBeat());
+                        s.WhenStarted(heartbeat => heartbeat.Start());
+                        s.WhenStopped(heartbeat => heartbeat.Stop());
+                    });
+
+                    x.RunAsLocalSystem();
+
+                    x.SetServiceName("HeartbeatService");
+                    x.SetDisplayName("Heartbeat Service");
+                    x.SetDescription("it will let you know that computer is alive or not");
+                });
+
+                int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+                Environment.ExitCode = exitCodeValue;
+
+
+            
+        }
+    }
+}
